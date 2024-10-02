4709aa187d4408bcd587f8248056abcd8567b0c2
Madelyn Winfrey <mwinfr@gmail.com>
2024-10-02 10:57:12 -0400

    adding files

---
+++ src/BigNumArithmetic.java
@@ -0,0 +1,21 @@
+import java.io.FileNotFoundException;
+import java.io.File;
+import java.util.Scanner;
+
+public class BigNumArithmetic {
+    public static void main(String[] args) throws Exception {
+        String fileName = args[0];
+
+        try{
+        File file = new File (fileName);
+        Scanner fileReader = new Scanner(file);
+        while (fileReader.hasNextLine()) {
+            String line = fileReader.nextLine();
+            System.out.println(line);
+        }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Invalid File");
+        }
+    }
+}
